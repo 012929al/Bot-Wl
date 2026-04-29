d5e056bdd1265130188778e581ab4a7d5bd62ca1
King_Patinhas <134103115+012929al@users.noreply.github.com>
2026-04-29 11:37:07 -0400

    Atualizar index.js
    Added a simple Express server that responds with 'Bot online!'

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 const fs = require('fs');
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Bot online!");
+});
+
+app.listen(3000, () => {
+    console.log("Servidor web ativo!");
+});
 
 const client = new Client({
     intents: [
